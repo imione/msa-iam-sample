d2e80a517640ce41633f982285127010f310f1ed
imione <sh.kim@modusign.co.kr>
2021-12-20 23:09:09 +0900

    feat: add configModule

--- apps/user-management/src/AppModule.ts
+++ apps/user-management/src/AppModule.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
+import { validationSchema } from '@apps/user-management/config/ValidationSchema';
+import DatabaseConfig from '@apps/user-management/config/DatabaseConfig';
 import { CoreModule } from '@apps/user-management/core/CoreModule';
 import { UserModule } from '@apps/user-management/user/UserModule';
 
+const configs = [DatabaseConfig];
+
 @Module({
-  imports: [CoreModule, UserModule],
+  imports: [
+    CoreModule,
+    ConfigModule.forRoot({
+      envFilePath: [
+        `${__dirname}/config/envs/.${process.env.NODE_ENV ??
+          'development'}.env`,
+      ],
+      load: [...configs],
+      isGlobal: true,
+      validationSchema,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
